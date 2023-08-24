dacbe7223545483b72b6a37eed6d6596955c1de1
sanjay321654 <sanjayilaya2002@gmail.com>
2023-08-24 21:03:25 +0530

    turtle-draw_random_walk.py

--- turtle-draw_random_walk.py
+++ turtle-draw_random_walk.py
@@ -1,13 +1,30 @@
-from turtle import Turtle
+import turtle
+from turtle import Turtle, Screen
 import random
-t = Turtle()
 
-colours = ["lime green", "dark orange", "green yellow"   , "gold", "dark violet", "red"]
+t = Turtle()
+turtle.colormode(255)
+
+
+# colours = ["lime green", "dark orange", "green yellow"   , "gold", "dark violet", "red"]
+def color_mode():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colors = (r, g, b)
+    return colors
 
 directions = [0, 90, 180, 270]
 
+t.pensize(10)
+
 for _ in range(200):
-    t.pensize(8)
+
     t.forward(30)
-    t.color(random.choice(colours))
+    t.color(color_mode())
     t.setheading(random.choice(directions))
+
+    t.speed("fastest")
+
+screen = Screen()
+screen.exitonclick()
